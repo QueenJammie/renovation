8267e5990ea1219992e4aa13e1c0f9e9840efd2d
QueenJammie <jammie.lacc@outlook.com>
2022-07-08 21:36:25 -0400

    Added a footer

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <div clasName="container">
-        <div className="row heading">
-          <h1>Sylvain Côté</h1>
-        </div>
+    <div clasName="container">
+      <div className="row heading">
+        <h1>Sylvain Côté</h1>
       </div>
+      <div className="row footer">
+        <a href="https://github.com/QueenJammie/renovation" target="_blank" rel="noreferrer" alt="OpenSource">Coded</a> by <a href="mailto:jammie.lacc@outlook.com">Jammie Lachance-Côté</a>
+      </div>
+    </div>
     </div>
   );
 }
